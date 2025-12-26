a83ab89a775b46d2950ec9a1f6d165a8620080c3
snehapaul05 <snehapaul312005@gmail.com>
2025-12-26 11:07:01 +0530

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,7 @@
-const products = [{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name :'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating : {
-  stars: 4.5,
-  count: 87
-  },
-  priceCents: 1090
-},{
-  image:'images/products/intermediate-composite-basketball.jpg',
-  name :'Intermediate Size Basketball',
-  rating : {
-  stars: 4.0,
-  count: 127
-  },
-  priceCents: 2095
-},{
-   image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name :' Adults Plain Cotton T-Shirt - 2 Pack',
-  rating : {
-  stars: 4.5,
-  count: 56
-  },
-  priceCents: 799
-}
-];
-
 let productHTML = '';
 
 products.forEach((products) => {
+   const usd_to_ind = 89.82;
   productHTML += `
   <div class="product-container">
           <div class="product-image-container">
@@ -48,7 +22,7 @@ products.forEach((products) => {
           </div>
 
           <div class="product-price">
-            $${(products.priceCents / 100).toFixed(2)}
+            ₹${((products.priceCents / 100)*usd_to_ind).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
