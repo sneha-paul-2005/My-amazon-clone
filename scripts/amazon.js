f04ccadcfdd09d33629afdeb9652a90c90cd66d2
snehapaul05 <snehapaul312005@gmail.com>
2025-12-26 21:35:36 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,6 +78,16 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
       quantity: 1
     });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerText = cartQuantity;
+
+    console.log(cartQuantity);
    
     console.log(cart);
   });
