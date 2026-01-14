e6fec5e47640e83d01c87b231fc05e12039fb755
snehapaul05 <snehapaul312005@gmail.com>
2026-01-14 14:03:58 +0530

    oop version of cart.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js"; 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import './cart-oop.js';
 
 renderOrderSummery();
 renderPaymentSummary();
