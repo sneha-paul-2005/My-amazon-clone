35139118a35a40ce109c88b2ce996ee768e244f5
snehapaul05 <snehapaul312005@gmail.com>
2026-01-15 23:53:31 +0530

    class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js"; 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import './cart-oop.js';
+import './cart-class.js';
 
 renderOrderSummery();
 renderPaymentSummary();
