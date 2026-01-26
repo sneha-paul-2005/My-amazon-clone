2ff6653b234dfd99c180991f78ee95c9fa7f4976
snehapaul05 <snehapaul312005@gmail.com>
2026-01-26 12:15:36 +0530

    classification done

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js"; 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import './cart-class.js';
+//import './cart-class.js';
 
 renderOrderSummery();
 renderPaymentSummary();
