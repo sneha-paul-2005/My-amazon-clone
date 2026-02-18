28de6632cfcb483bac695c15cc4d7655de12e1a3
snehapaul05 <snehapaul312005@gmail.com>
2026-02-18 21:11:23 +0530

    use of promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,50 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import './cart-class.js';
 //import '../data/backhand-practice.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "./cart.js";
 
+Promise.all ([
+  new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  }); 
+}),
+
+new Promise((resolve) => {
+  loadCart(() => {
+    resolve();
+  });
+})
+
+]).then(() => {
+  renderOrderSummery();
+  renderPaymentSummary();
+});
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  }); 
+
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummery();
+  renderPaymentSummary();
+});
+*/
+
+
+/*
 loadProducts(() => {
   renderOrderSummery();
   renderPaymentSummary();
 });
+*/
